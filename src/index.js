1253922511b9a88d3309104e9ffc4e9cafcec16c
zowietao <zowietao@gmail.com>
2023-03-18 18:08:57 +0800

    first meet to flow

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+// Preferences: Open User Settings
+// "javascript.validate.enable": false,
+
+function concat(a: number, b: number) {
+  return a + b;
+}
+
+concat("A", "B"); // Works!
+concat(1, 2); // Error!
